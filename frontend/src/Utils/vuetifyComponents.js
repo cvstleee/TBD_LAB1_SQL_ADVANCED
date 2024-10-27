b7dd7aa11cef29f1c77fd56c4cb1601aa95052a7
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2024-10-27 20:05:25 -0300

    refactor: change the vue2 to vue3 and fix the vuetify error in components

---
+++ frontend/src/Utils/vuetifyComponents.js
@@ -0,0 +1,10 @@
+import {
+  VApp,
+  VAppBar,
+  VToolbarTitle,
+  VSpacer,
+  VBtn,
+  VIcon,
+} from "vuetify/components";
+
+export { VApp, VAppBar, VToolbarTitle, VSpacer, VBtn, VIcon };
--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,39 +1,29 @@
 <template>
-    <div>
-      <Navbar />
-      <h1>Productos</h1>
-      <ul>
-        <li v-for="product in products" :key="product.id">{{ product.name }}</li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  import Navbar from './components/NavBar.vue'; // Asegúrate de que la ruta sea correcta
-  import productService from './services/productService';
-  
-  export default {
-    components: {
-      Navbar,
-    },
-    data() {
-      return {
-        products: [],
-      };
-    },
-    async created() {
-      try {
-        this.products = await productService.getProducts();
-      } catch (error) {
-        console.error(error.message);
-      }
-    },
-  };
-  </script>
-  
-  <style scoped>
-  /* Estilos específicos para Home.vue */
-  body {
-    background-color: #f4f4f4; /* Ejemplo de estilo */
+  <div>
+    <h1>Productos</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">{{ product.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import productService from '../services/productService';
+
+const products = ref([]);
+
+onMounted(async () => {
+  try {
+    products.value = await productService.getProducts();
+  } catch (error) {
+    console.error(error.message);
   }
-  </style>
+});
+</script>
+
+<style scoped>
+body {
+  background-color: #f4f4f4;
+}
+</style>
