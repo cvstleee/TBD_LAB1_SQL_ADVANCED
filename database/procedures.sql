47b8db886998be4780459a60d6114392b493e4c3
Alonso Sanhueza <alonso.sanhueza32@gmail.com>
2024-11-23 01:52:56 -0300

    refactor(DB): move shipping_date to orders and complete procedures

--- database/procedures.sql
+++ database/procedures.sql
@@ -1,19 +1,40 @@
-
--- Desactivar un producto cuando su stock llegue a cero
-CREATE OR REPLACE PROCEDURE deactivate_product_with_zero_stock()
-LANGUAGE SQL
+CREATE OR REPLACE PROCEDURE update_product(
+    p_id INT,
+    p_name VARCHAR(255),
+    p_description TEXT,
+    p_price DECIMAL(10, 2),
+    p_stock INT,
+    p_state VARCHAR(50),
+    p_deleted_at TIMESTAMP
+) 
+LANGUAGE plpgsql
 AS $$
-    UPDATE products 
-    SET state = 'no disponible'
-    WHERE stock = 0;
+DECLARE
+    v_state VARCHAR(50);
+BEGIN
+    IF p_stock <= 0 THEN
+        v_state := 'not available';
+    ELSE 
+        v_state := p_state;
+    END IF;
+
+    UPDATE products
+    SET name = p_name,
+        description = p_description,
+        price = p_price,
+        stock = p_stock,
+        state = v_state
+    WHERE id = p_id AND deleted_at IS NULL;
+END;
 $$;
 
-
 -- Actualizar precios de los productos de una categoria en base a un porcentaje de incremento
-CREATE OR REPLACE PROCEDURE update_products_prices_with_rate(rate NUMERIC, category_name VARCHAR)
-LANGUAGE SQL
+CREATE OR REPLACE PROCEDURE update_products_prices_with_rate(rate NUMERIC, category_id INT)
+LANGUAGE plpgsql
 AS $$
+BEGIN
     UPDATE products
-    SET price = price * rate
-    WHERE products.category_id = (SELECT id FROM categories WHERE name = category_name);
+    SET price = price * (1 + (rate / 100))
+    WHERE products.category_id = category_id;
+END;
 $$;
